fb84df36fc4c235b29cb2762db0a64059f350da0
Peter Edge <peter@centzy.com>
2013-08-07 17:45:05 -0400

    change class variables to instance variables on Enum

--- lib/protocol_buffers/runtime/enum.rb
+++ lib/protocol_buffers/runtime/enum.rb
@@ -4,30 +4,30 @@ module ProtocolBuffers
     extend self
 
     def set_fully_qualified_name(fully_qualified_name)
-      @@fully_qualified_name = fully_qualified_name.dup.freeze
+      @fully_qualified_name = fully_qualified_name.dup.freeze
     end
 
     def fully_qualified_name
-      @@fully_qualified_name
+      @fully_qualified_name
     end
 
     def value_to_names_map
-      @@value_to_names_map ||= self.constants.inject(Hash.new) do |hash, constant|
+      @value_to_names_map ||= self.constants.inject(Hash.new) do |hash, constant|
         # values do not have to be unique
         value = self.const_get(constant)
         hash[value] ||= Array.new
         hash[value] << constant
         hash
       end
-      @@value_to_names_map
+      @value_to_names_map
     end
 
     def name_to_value_map
-      @@name_to_value_map ||= self.constants.inject(Hash.new) do |hash, constant|
+      @name_to_value_map ||= self.constants.inject(Hash.new) do |hash, constant|
         hash[constant] = self.const_get(constant)
         hash
       end
-      @@name_to_value_map
+      @name_to_value_map
     end
   end
 end
