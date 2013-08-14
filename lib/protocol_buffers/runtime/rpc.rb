fe558a39381c3bb9663fa77e925c8f15e0bfd3e7
Peter Edge <peter@centzy.com>
2013-08-14 12:41:07 -0400

    move hash for RPC#to_s to to_s

--- lib/protocol_buffers/runtime/rpc.rb
+++ lib/protocol_buffers/runtime/rpc.rb
@@ -7,15 +7,15 @@ module ProtocolBuffers
       @proto_name = proto_name.dup.freeze
       @request_class = request_class
       @response_class = response_class
-      @hash = {
+    end
+
+    def to_s
+      @hash ||= {
         :name => name,
         :proto_name => proto_name,
         :request_class => request_class,
         :response_class => response_class
       }.freeze
-    end
-
-    def to_s
       @hash.to_s
     end
   end
