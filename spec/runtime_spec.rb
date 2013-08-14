d7885df21b0eb8a914c2948ba4a5ce9e65781ee9
Peter Edge <peter@centzy.com>
2013-08-14 15:47:20 -0400

    check names on rpcs

--- spec/runtime_spec.rb
+++ spec/runtime_spec.rb
@@ -801,9 +801,7 @@ describe ProtocolBuffers, "runtime" do
   end
 
   it "correctly handles service definitions" do
-    Services::FooBarService.rpcs.size.should == 2
-    get_foo_rpc = Services::FooBarService.rpcs[0]
-    get_bar_rpc = Services::FooBarService.rpcs[1]
+    get_foo_rpc, get_bar_rpc = get_rpcs
 
     get_foo_rpc.name.should == :get_foo
     get_foo_rpc.proto_name.should == "GetFoo"
@@ -819,9 +817,7 @@ describe ProtocolBuffers, "runtime" do
   end
 
   it "correctly handles == for Rpcs" do
-    Services::FooBarService.rpcs.size.should == 2
-    get_foo_rpc = Services::FooBarService.rpcs[0]
-    get_bar_rpc = Services::FooBarService.rpcs[1]
+    get_foo_rpc, get_bar_rpc = get_rpcs
 
     get_foo_rpc.should == get_foo_rpc
     get_bar_rpc.should == get_bar_rpc
@@ -829,9 +825,7 @@ describe ProtocolBuffers, "runtime" do
   end
 
   it "correctly freezes rpcs" do
-    Services::FooBarService.rpcs.size.should == 2
-    get_foo_rpc = Services::FooBarService.rpcs[0]
-    get_bar_rpc = Services::FooBarService.rpcs[1]
+    get_foo_rpc, get_bar_rpc = get_rpcs
 
     get_foo_rpc.frozen?.should == true
     get_bar_rpc.frozen?.should == true
@@ -844,5 +838,20 @@ describe ProtocolBuffers, "runtime" do
   it "correctly handles fully qualified names on Services" do
     Services::FooBarService.fully_qualified_name.should == "services.FooBarService"
     Services::NoNameFooBarService.fully_qualified_name.should == nil
+  end
+
+  def get_rpcs
+    Services::FooBarService.rpcs.size.should == 2
+    first_rpc = Services::FooBarService.rpcs[0]
+    second_rpc = Services::FooBarService.rpcs[1]
+    case first_rpc.name
+    when :get_foo
+      second_rpc.name.should == :get_bar
+      return first_rpc, second_rpc
+    when :get_bar
+      first_rpc.name.should == :get_bar
+      return second_rpc, first_rpc
+    else raise ArgumentError.new(first_rpc.name)
+    end
   end
 end
