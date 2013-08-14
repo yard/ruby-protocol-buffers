0f1f91b7731c641dfff351ee2c8e25e8de572618
Peter Edge <peter@centzy.com>
2013-08-14 12:57:16 -0400

    changes to Rpc and Service, services.proto and services.pb.rb in spec/proto_files/

--- lib/protocol_buffers/runtime/rpc.rb
+++ lib/protocol_buffers/runtime/rpc.rb
@@ -1,20 +1,37 @@
 module ProtocolBuffers
   class Rpc
-    attr_reader :name, :proto_name, :request_class, :response_class
+    attr_reader :name, :proto_name, :request_class, :response_class, :service_class
 
-    def initialize(name, proto_name, request_class, response_class)
+    def initialize(name, proto_name, request_class, response_class, service_class)
       @name = name
       @proto_name = proto_name.dup.freeze
       @request_class = request_class
       @response_class = response_class
+      @service_class = service_class
+    end
+
+    def eql?(other)
+      self.class.equal?(other.class) &&\
+        name == other.name &&\
+        proto_name == other.proto_name &&\
+        request_class == other.request_class &&\
+        response_class == other.response_class &&\
+        service_class == other.service_class
+    end
+
+    alias == eql?
+
+    def hash
+      name.hash ^ proto_name.hash ^ request_class.hash ^ response_class.hash ^ service_class.hash
     end
 
     def to_s
       @hash ||= {
         :name => name,
         :proto_name => proto_name,
-        :request_class => request_class,
-        :response_class => response_class
+        :request_class_name => request_class.name,
+        :response_class_name => response_class.name,
+        :service_class_name => service_class.name
       }.freeze
       @hash.to_s
     end
